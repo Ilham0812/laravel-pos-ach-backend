db9b957042a48ea6f8d8205f5818908a4bdf0efb
ilham0812 <ilham25951@gmail.com>
2024-02-15 09:54:09 +0700

    init ach backend

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Project Resto</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard -
+                        Resto</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link" href="{{ url('dashboard-general-dashboard') }}">General Dashboard
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('users.index') }}">User Dashboard</a>
+                    </li>
+                </ul>
+            </li>
+</div>
